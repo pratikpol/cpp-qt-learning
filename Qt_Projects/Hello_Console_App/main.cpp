edb56624732009aa976dfd5d3988aa7e19633dcc
pratikpol <pratikkpol@gmail.com>
2025-06-02 12:02:49 +0530

    Added a simple Hello_Console_App program

---
+++ Qt_Projects/Hello_Console_App/main.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    qDebug() <<"Hello World!";
+
+    //return a.exec();
+    return 0;
+}
