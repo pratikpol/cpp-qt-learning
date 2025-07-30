f43b8a6e1bb69699be54fbc5a71f543f07ba418b
pratikpol <pratikkpol@gmail.com>
2025-07-30 16:02:31 +0530

    Sample output added in brackets infront of comment to understand

--- CppBasics/Pointer/main.cpp
+++ CppBasics/Pointer/main.cpp
@@ -7,12 +7,12 @@ int main()
     string Airbus = "Toulouse";
     string* ptr = &Airbus;
 
-    cout << Airbus << "\n";   //Output the value of Airbus
+    cout << Airbus << "\n";   //Output the value of Airbus (Toulouse)
 
-    cout << &Airbus << "\n";  //Output the memory address of Airbus
+    cout << &Airbus << "\n";  //Output the memory address of Airbus (0x48aa1ffd80)
 
-    cout << ptr << "\n";      //Output the memory address with pointer
+    cout << ptr << "\n";      //Output the memory address with pointer (0x48aa1ffd80)
 
-    cout << *ptr << "\n";     //Output the value with pointer
+    cout << *ptr << "\n";     //Output the value with pointer (Toulouse)
     return 0;
 }
