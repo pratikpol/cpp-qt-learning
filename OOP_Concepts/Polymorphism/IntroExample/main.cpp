8519681fa945ddc075d9c0a8a2521bf9159cffb3
pratikpol <pratikkpol@gmail.com>
2026-06-04 17:41:18 +0530

    Simple Introductory example of Polymorphism

---
+++ OOP_Concepts/Polymorphism/IntroExample/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Aircraft{
+public:
+    string type;
+
+    Aircraft(){
+        cout << "non-parameterized constructor"<<endl;
+    }
+
+    Aircraft(string type){
+        this->type = type;
+        cout <<"parameterized constructor"<<endl;
+    }
+
+};
+
+int main()
+{
+    Aircraft a;  //It will call non-parameterized constructor
+
+    return 0;
+}
+
+
+
+//Its an example of function overloading, where function shared same name but different parameters and operations
+//Poly means many or multiple
+//Morph means forms or shapes
